4bc168ba8e82b73d7c4ffbb8e7ab415a5440e426
Marco Stolle <marco.stolle@gmail.com>
2026-04-20 20:37:50 +0200

    updated timezone in nixos-common.nix

--- 2025-02-nix-nvidia-ollama/nix/hosts/common/nixos-common.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/common/nixos-common.nix
@@ -3,7 +3,7 @@ let
   inherit (inputs) nixpkgs nixpkgs-unstable;
 in
 {
-  time.timeZone = "America/New_York";
+  time.timeZone = "Belgium/Brussels";
   system.stateVersion = "24.11";
 
   virtualisation = {
